77669ba9e4fb8837237567d0fee63583b1fc7ed4
Marc Emmanuel <marc.emmanuel@virtual-identity.com>
2019-01-10 10:05:42 +0100

    Add common biotope element bundle loading

--- projectConfig.js
+++ projectConfig.js
@@ -5,10 +5,6 @@ module.exports = {
 			'vi-css-helper': ['helper.css', 'print.css'],
 			'sanitize.css': 'sanitize.css',
 			'handlebars': 'dist/handlebars.runtime.js'
-		},
-		tsEntryPoints: [
-			'resources/ts/**/*.ts',
-			'components/**/*.ts'
-		]
+		}
 	}
 };
